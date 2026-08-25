d1eeae1e68ad16b012cbfd3072cdbcc202b9e51f
karoljaro <111643413+karoljaro@users.noreply.github.com>
2026-08-25 06:07:14 +0200

    feat: Implement FaceReactionPipeline for processing face landmarking

---
+++ src/vision/face/pipeline/face_reaction_pipeline.py
@@ -0,0 +1,23 @@
+from collections.abc import Iterator, Iterable
+import mediapipe as mp
+from cv2.typing import MatLike
+from vision.async_landmark_processor import AsyncLandmarkProcessor
+from mediapipe.tasks.python.vision import (
+    FaceLandmarkerResult,
+    FaceLandmarkerOptions,
+    FaceLandmarker
+)
+from vision.face.face_landmarker import create_face_landmarker_options
+
+
+class FaceReactionPipeline:
+    def __init__(self) -> None:
+        self._face_landmarker = AsyncLandmarkProcessor[FaceLandmarkerResult, FaceLandmarkerOptions](
+            FaceLandmarker, create_face_landmarker_options
+        )
+
+    def process(
+        self, stream: Iterable[tuple[MatLike, mp.Image, int]]
+    ) -> Iterator[tuple[MatLike, FaceLandmarkerResult, int]]:
+        detection_stream = self._face_landmarker.process(stream)
+        yield from detection_stream
