80dbac91ede9c79f28d82eb68f8464d14a03c69b
karoljaro <111643413+karoljaro@users.noreply.github.com>
2026-09-05 15:35:35 +0200

    refactor: Add configuration options for hand detection and tracking in create_hand_landmarker_options

--- src/vision/hand/hand_landmarker.py
+++ src/vision/hand/hand_landmarker.py
@@ -16,5 +16,9 @@ def create_hand_landmarker_options(
             model_asset_path="models/hand_landmarker.task",
         ),
         running_mode=VisionTaskRunningMode.LIVE_STREAM,
+        num_hands=1,
+        min_hand_detection_confidence=0.7,
+        min_hand_presence_confidence=0.7,
+        min_tracking_confidence=0.6,
         result_callback=callback,
     )
