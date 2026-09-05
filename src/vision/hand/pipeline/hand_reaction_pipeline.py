c664d599c01bce4455c59b7574d7d595ac1b8c0a
karoljaro <111643413+karoljaro@users.noreply.github.com>
2026-09-05 15:21:08 +0200

    refactor: Update HandReactionPipeline to use ClassifierResult and remove Gesture enum

--- src/vision/hand/pipeline/hand_reaction_pipeline.py
+++ src/vision/hand/pipeline/hand_reaction_pipeline.py
@@ -3,17 +3,16 @@ from mediapipe.tasks.python.vision.hand_landmarker import HandLandmarkerResult
 from vision.hand.hand_gesture_classifier import HandGestureClassifier
 from vision.classification_stabilizer import Stabilizer
 from vision.hand.hand_pose_analyzer import HandPoseAnalyzer
-from vision.hand.types.gesture import Gesture
+from vision.types import ClassifierResult
 
 from collections.abc import Callable
 
 
 class HandReactionPipeline:
-    def __init__(self, on_gesture: Callable[[Gesture, int], None]) -> None:
+    def __init__(self, on_gesture: Callable[[ClassifierResult, int], None]) -> None:
         self._pose_analyzer = HandPoseAnalyzer()
         self._classifier = HandGestureClassifier()
-        self._stabilizer = Stabilizer[HandLandmarkerResult]()
-        # FIX: FIX GESTURE TYPING FROM PURE LITERAL TO ENUM
+        self._stabilizer = Stabilizer[ClassifierResult]()
         self._on_gesture = on_gesture
 
     def handle_result(
@@ -25,7 +24,7 @@ class HandReactionPipeline:
         classified_gesture = self._classifier.classify(finger_states)
         stabilized_gesture = self._stabilizer.stabilize(
             classified_gesture,
-            lambda gesture: gesture is Gesture.UNKNOWN
+            lambda gesture: gesture == "UNKNOWN"
         )
 
         if stabilized_gesture is not None:
--- src/vision/hand/types/gesture.py
+++
@@ -1,9 +0,0 @@
-from enum import StrEnum
-
-
-class Gesture(StrEnum):
-    FIST = "FIST"
-    OPEN_PALM = "OPEN_PALM"
-    POINTING = "POINTING"
-    PEACE = "PEACE"
-    UNKNOWN = "UNKNOWN"
